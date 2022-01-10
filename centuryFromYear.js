eb21239eaaad124bfb154c44b30606fb0b6b3088
Olivia Bloway <blowayol@msu.edu>
2022-01-09 22:02:29 -0500

    more practice

---
+++ centuryFromYear.js
@@ -0,0 +1,12 @@
+function century(year) {
+    let roundedNumber =  year/100
+     return Math.ceil(roundedNumber)
+   }
+   
+   // i am gonna get a year
+   // i have to return the century the year is in 
+   // will i only get intergers/ stings that might convert?
+   // will i get year zero
+   // will i get any null years or undefied 
+   //2119 - 22 centyrt
+   // im gonna take the year/100 and wrap it in Math.round()
