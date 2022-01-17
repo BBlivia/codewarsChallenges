2b05cb14b65cc0fb6f9c7f1f6deb69c88e386910
Olivia Bloway <blowayol@msu.edu>
2022-01-16 22:11:17 -0500

    coding challenge and solution

---
+++ invertedValues.js
@@ -0,0 +1,12 @@
+function invert(array) {
+    let newArr = array.map(num => num * -1)
+      return newArr;
+   }
+   
+   // i will be getting an array of numbers
+   // - will i be getting any fractins will they only be wholes 
+   // - i need to return a new array bakc with the elements inverted 
+   // [1, -4, 6] => [-1, 4, -6]
+   // i am going to interate through the array
+   // im gonna take not of the element and inverted 
+   // then im going to return a new array with the all the newly inverted elements 
