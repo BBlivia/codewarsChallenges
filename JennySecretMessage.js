007ea99d91ca80e245c4099b9d54014dc7f04398
Olivia Bloway <blowayol@msu.edu>
2022-01-18 22:13:08 -0500

    coding challenge and solution

---
+++ JennySecretMessage.js
@@ -0,0 +1,13 @@
+//Jenny has written a function that returns a greeting for a user. 
+//However, she's in love with Johnny, and would like to greet him slightly different. 
+//She added a special case to her function, but she made a mistake.
+//Can you help her?
+
+
+function greet(name){
+  
+    return name === 'Johnny'? 'Hello, my love!': `Hello, ${name}!`
+  }
+  
+  // im gonna get a name i need to return hello plus the name unless it's johnn i need to say hello my loe
+  //
