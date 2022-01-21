a23334d49f46baea00fbf71062e648eca09ff2ea
Olivia Bloway <blowayol@msu.edu>
2022-01-20 22:13:50 -0500

    coding challenge and solution

---
+++ MathOps.js
@@ -0,0 +1,19 @@
+function basicOp(operation, value1, value2)
+{ if(operation === '+'){
+ return  value1 + value2
+}else if(operation === '-'){
+  return value1 - value2
+}else if(operation === '/'){
+  return value1 / value2
+}else{
+ return  value1 * value2
+}
+ 
+}
+
+// i am gonna get 2 values and an operation
+// i have to return the vlaue after performing the operation on my two vlaues\
+// will i get any null or 0;
+// will i be gettting floated numbers
+// - 3, 7 => 3 -7 return -4
+// im seeing i have to do 4 if statements 
