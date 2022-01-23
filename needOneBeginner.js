d3fc71e5e969b22581b0bdfedc355475739af31d
Olivia Bloway <blowayol@msu.edu>
2022-01-22 22:14:38 -0500

    coding challenge and solution

---
+++ needOneBeginner.js
@@ -0,0 +1,22 @@
+/*You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+
+ */
+
+
+
+function check(a, x) {
+    return a.includes(x)? true:false
+    }
+    
+    // i am going to get 2 params the first is an array the second is a value
+    // i need to return true or falase depending on if the array contains the value 
+    // will i get an empty array;
+    // will the value be any datatype 
+    // will the array be of mixed data types
+    // i need to iterate through the array and look for the value
+    // return true if it's there
+    // false if not 
