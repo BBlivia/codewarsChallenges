a792d9da1217fc689413be484ba355ff55810f3b
Olivia Bloway <blowayol@msu.edu>
2022-01-25 22:15:26 -0500

    coding challenge and solution

---
+++ numberToString.js
@@ -0,0 +1,18 @@
+/*We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100" */
+
+
+function numberToString(num) {
+    return '' + num
+  }
+  
+  // i a gonna get a number, is it only interes, will i get floats,?
+  // i have to return that number stringy 
+  //E- 563= '563'
+  // return stringfy(num), i could just concat it with ' '
