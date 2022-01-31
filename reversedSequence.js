7f3974b01f910f596f0be6143ffd6a3525a3905c
Olivia Bloway <blowayol@msu.edu>
2022-01-30 22:17:24 -0500

    coding challenge and solution

---
+++ reversedSequence.js
@@ -0,0 +1,14 @@
+const reverseSeq = n => {
+    let arr = [];
+    for(let i = n; i>=1; i--){
+    arr.push(i)
+    } return arr
+  };
+  
+  
+  // i am gonna get a number
+  // i am gonna return an array with that number and all the others that come before it in an array
+  // n = 4 =>[4, 3, 2, 1]
+  // im gonna create an empty array
+  // im gonna loop through to n 
+  // making sure to go down
