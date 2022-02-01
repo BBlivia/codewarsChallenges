52673a82b74db587a325ab8b757c998536023d1f
Olivia Bloway <blowayol@msu.edu>
2022-01-31 22:17:44 -0500

    coding challenge and solution

---
+++ reverseListOrder.js
@@ -0,0 +1,22 @@
+/*In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+ */
+
+
+function reverseList(list) {
+    console.log(`i am the ${list}`)
+     list.reverse()
+  
+    return list
+    
+  }
+  
+  
+  //i will get an array 
+  // will the elements in the array only be intergers and no other data?
+  // i have ot retrn the same array bt reversed 
+  //i can se the reverse method 
+  
