6c739ce3e4ad8cd6493eda253a6a88b2f9cc49ad
Olivia Bloway <blowayol@msu.edu>
2022-02-23 15:32:12 -0500

    takes in an array and removes comma

---
+++ arrayWithCommaEliminated.js
@@ -0,0 +1,9 @@
+function printArray(array){
+    return array.join()
+  }
+  
+  
+  
+  // i will be getting an input of an array (string)
+  // I have to return the string with the commas eleminated, make the string a string with no inside commas 
+  // I willjoin them 
