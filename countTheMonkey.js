8c3dbcee9ea41c374cffaed5fcba4a1c907498de
Olivia Bloway <blowayol@msu.edu>
2022-03-11 15:40:08 -0500

    conpleted challenge

---
+++ countTheMonkey.js
@@ -0,0 +1,23 @@
+function monkeyCount(n) {
+    let arr = []
+    for(let i = 1; i<= n; i++){
+    arr.push(i)
+    }return arr
+  }
+  
+  
+  // im gonna get a number "n" 
+  //- will n ever be a negative number 
+  // will n ever be 0;
+  // will n only be a number 
+  
+  // im gonna have to return an array of numbers from 1 to n
+  // ex: 5 = [1, 2, 3, 4 ,5 ]
+  // 3 =[ 1, 2 ,3]
+  // start
+  // im gonna create an empty array 
+  // I'm gonna have an input n
+  // i want to count from 1 until i reach n including n 
+  // i will push numbers to array
+  // return the array
+  //end
