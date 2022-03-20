e8413ddc3b429faf78d32c136b96b652d997330b
Olivia Bloway <blowayol@msu.edu>
2022-03-20 17:12:23 -0400

    finished

---
+++ reversetheString.js
@@ -0,0 +1,18 @@
+/*Complete the solution so that it reverses all of the words within the string passed in.
+Example:
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"*/
+
+
+
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ')
+  }
+  
+  
+  // they are gonna give me a stirng, all words 
+  // the words are backwards not the letter
+  // have to return the same sting but backwords 
+  // " i want this" 'this want i 
+  // 
+
